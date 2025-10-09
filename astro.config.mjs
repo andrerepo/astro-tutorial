a34786a1695de9ce8804f31a4c3d141fbcba0bd5
Andre Rodrigues <andrerepo@pm.me>
2025-10-09 00:15:38 -0300

    new: rss feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://iridescent-bubblegum-4ab688.netlify.app/",
+});
